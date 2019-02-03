f204397849a151e335bace4da923cba3425338ee
Paulius Zaranka <Paulius.z15@gmail.com>
2019-02-03 19:25:42 +0200

    Updates

--- precache-manifest.1817f5a9b861c2e99ffada4dce090b7f.js
+++ precache-manifest.1817f5a9b861c2e99ffada4dce090b7f.js
@@ -32,12 +32,12 @@ self.__precacheManifest = [
     "url": "/energy-meter/static/media/flags.9c74e172.png"
   },
   {
-    "revision": "701ae6abd4719e9c2ada3535a497b341",
-    "url": "/energy-meter/static/media/outline-icons.701ae6ab.eot"
-  },
-  {
     "revision": "ad97afd3337e8cda302d10ff5a4026b8",
     "url": "/energy-meter/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/energy-meter/static/media/outline-icons.701ae6ab.eot"
   },
   {
     "revision": "0ab54153eeeca0ce03978cc463b257f7",
@@ -60,16 +60,16 @@ self.__precacheManifest = [
     "url": "/energy-meter/static/media/icons.8e3c7f55.eot"
   },
   {
-    "revision": "13db00b7a34fee4d819ab7f9838cc428",
-    "url": "/energy-meter/static/media/brand-icons.13db00b7.eot"
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/energy-meter/static/media/brand-icons.c5ebe0b3.ttf"
   },
   {
     "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
     "url": "/energy-meter/static/media/icons.b87b9ba5.ttf"
   },
   {
-    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
-    "url": "/energy-meter/static/media/brand-icons.c5ebe0b3.ttf"
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/energy-meter/static/media/brand-icons.13db00b7.eot"
   },
   {
     "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/energy-meter/precache-manifest.830b6843860c09d3bb799a340ee86ba4.js"
+  "/energy-meter/precache-manifest.1817f5a9b861c2e99ffada4dce090b7f.js"
 );
 
 workbox.clientsClaim();
